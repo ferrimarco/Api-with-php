09198404504bb754884e8297ca84777500752625
ferrimarco <116425965+ferrimarco@users.noreply.github.com>
2024-03-13 09:46:26 +0100

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
+<?php
+    $users = [
+        ['id' => 1, 'name' => 'Marco', 'surname' => 'Ferri', 'age' => 18],
+        ['id' => 2, 'name' => 'Marco', 'surname' => 'Piteo', 'age' => 18],
+        ['id' => 3, 'name' => 'Cristian', 'surname' => 'Pesola', 'age' => 18],
+        ['id' => 4, 'name' => 'Damiano', 'surname' => 'Favale', 'age' => 18]
+    ];
+
+    http_response_code(200);
+    header('Content-Type: application/json');
+
+    // primo formato
+    // echo json_encode($users);
+
+    //secondo formato
+    echo json_encode([
+        "status" => 200,
+        "message" => "Success",
+        "payload" => $users
+    ]); 
+    
+    exit;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
